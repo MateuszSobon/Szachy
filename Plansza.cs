e9b3287475c37ff8b444c6a752a87606e63778c5
Mateusz Sobon <mateusz17230@gmail.com>
2023-05-06 23:36:54 +0200

    ogranicznik czasu, educja wyglądu

--- Plansza.cs
+++ Plansza.cs
@@ -4,30 +4,77 @@ class Szachownica
 
     int tura; //zlicza ile już tur zosrtało wykonanych
 
+    public string kto_gra(int t)
+    {
+        if (t%2==0)
+        {
+            return "białe";
+        }
+        else
+        {
+            return "czarne";
+        }
+            
+    }
+    public void nowa_plansza()
+    {
+        tura++;
+    }
     public void wypisz()
     {   
-        Console.WriteLine("Tu będzie wyświetlony nagłówek");
-        Console.WriteLine("---------------------------------");
-        
+        Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.WriteLine($"Tura: {tura}    Grają {kto_gra(tura)}");
+        Console.BackgroundColor = ConsoleColor.DarkGray;
+        Console.Write("  ");
 
+        for(int i=0; i<8; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(" |");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($" {i}");
+        }
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.Write(" |"); 
+        Console.WriteLine("\n------------------------------------");
+        
         for(int i=0; i<plansza.GetLength(0); i++)
         
-        {
-            Console.Write("|");  
+        {   
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write($" {i}");  
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(" |"); 
+
             for(int j=0; j<plansza.GetLength(1); j++)
             {
-                Console.Write($"{plansza[i,j].nazwa}|");
-                
+                if (plansza[i,j].kolor==Kolor.biały)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else if (plansza[i,j].kolor==Kolor.czarny)
+                {
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+
+                Console.Write(plansza[i,j].nazwa);
+
+                Console.ForegroundColor = ConsoleColor.DarkGreen;
+                Console.Write("|");
             }
-            Console.WriteLine("\n---------------------------------");
+            Console.WriteLine("\n------------------------------------");
         }
+        Console.ResetColor();
     }
     public Szachownica()
     {   
+        tura = 0;
+
         for (int i=0; i<8; i++)
         {
-            plansza[1,i]=new Pionek(Kolor.czarny);
-            plansza[6,i]=new Pionek(Kolor.biały);
+            plansza[1,i]=new Pionek(Kolor.biały);
+            plansza[6,i]=new Pionek(Kolor.czarny);
 
             plansza[2,i]=new Puste();
             plansza[3,i]=new Puste();
--- Program.cs
+++ Program.cs
@@ -1,9 +1,31 @@
-﻿internal class Program
+﻿using System.Diagnostics;
+using System.Threading;
+internal class Program
 {
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("Cześć, zagramy w szachy. Żeby poznać zasady wpisz \"help\" lub wciśnij enter żeby zagrać.");
+        var napis = Console.ReadLine();
+        // sprawdz czy nie wpisano help
         Szachownica szachownica = new Szachownica();
+
         szachownica.wypisz();
+        Stopwatch stoper = new Stopwatch();
+        stoper.Start();
+
+        while(stoper.Elapsed.TotalSeconds<100)
+        {   
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0,1);
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine($"Pozostały czas: {100-Convert.ToInt32(stoper.Elapsed.TotalSeconds)}s  (aby odświerzyć zerag wciśnij enter)");
+            Console.ResetColor();
+            Console.SetCursorPosition(0,20);
+            Console.CursorVisible = true;
+
+            Console.ReadLine();
+        }
+        
+
     }
 }
