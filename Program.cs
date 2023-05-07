6d4d523c2812844dea994c384ee50427058b6e84
Mateusz Sobon <mateusz17230@gmail.com>
2023-05-07 10:05:40 +0200

    przyjmowanie cord do ruchu

--- Program.cs
+++ Program.cs
@@ -12,6 +12,8 @@ internal class Program
         szachownica.wypisz();
         Stopwatch stoper = new Stopwatch();
         stoper.Start();
+        string? napis2;
+        int x,y;
 
         while(stoper.Elapsed.TotalSeconds<100)
         {   
@@ -20,12 +22,22 @@ internal class Program
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
             Console.WriteLine($"Pozostały czas: {100-Convert.ToInt32(stoper.Elapsed.TotalSeconds)}s  (aby odświerzyć zerag wciśnij enter)");
             Console.ResetColor();
-            Console.SetCursorPosition(0,20);
+            Console.SetCursorPosition(0,21);
+            Console.WriteLine("                     ");
+            Console.SetCursorPosition(0,21);
             Console.CursorVisible = true;
 
-            Console.ReadLine();
+            napis2 = Console.ReadLine();
+            if(napis2!=null)
+            {
+                string[] cor = napis2.Split(',');
+                if( cor.Length==2 && int.TryParse(cor[0], out x) && int.TryParse(cor[1],out y) )
+                {
+                    break;
+                }
+            }
         }
-        
-
+        Console.Write("koniec gry");
+        Console.Read();
     }
 }
