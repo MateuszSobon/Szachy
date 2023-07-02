b6128b926ebc8c00725cf9bdcc0c5b23e372438b
Mateusz Sobon <mateusz17230@gmail.com>
2023-07-02 13:22:39 +0200

    Dodanie funkcji help 1.0

--- Program.cs
+++ Program.cs
@@ -2,17 +2,57 @@
 using System.Threading; 
 
 // LIST ++++++++++++++++++++++
-// resetujący się timer jak po wybraniu pionka do ruchu wprowadzi się błędne współrzędne !!!OK
 // funkcjinalność wymuszająca ruch krola ktory może zostać zbity
 // tryb gry jednoosobowej
 // menu help
-//po skończonym czasie na ruch nie zmienia się tura
+
 internal class Program
 {   
     private static void help()
-    {
-        Console.WriteLine("Póki co tu nic nie ma, aby wyjść z help wciśnij dowolny klawisz");
-        Console.ReadLine();
+    {   
+        string? wybrana;
+        int dana;
+        Console.WriteLine("\n\nWitaj w helpie. Znajdziesz tutaj zasady i porady dotyczące aplikacji Szachy \n");
+        Console.WriteLine("Gra posiada 2 tryby: jednoosobowy i dwuosobowy");
+        Console.WriteLine("Aby poczytać o tyrbie 1 os naciśnij \"1\" Aby poczytać o tyrbie 1 os naciśnij \"2\": \n");
+        try
+        {
+            wybrana = Console.ReadLine(); // do poprawy dla bezpieczeństwa
+            if (int.TryParse(wybrana, out dana) || dana==1)
+            {
+                Console.WriteLine("Gra jeszcze niedostępna");
+                Console.WriteLine("Aby opuścić help naciśnij dowlolny klawisz");
+                Console.ReadLine();
+            }
+            else if (int.TryParse(wybrana, out dana) || dana==2)
+            {
+                Console.WriteLine("\n Gra polega na naprzemiennym wybieraniu wpółrzędnych pionków którymi gracz chce się ruszyć. \n  Każdy pionek ma indywidualne możliwości ruchu.\n");
+                Console.WriteLine("1: Wybierz pionek którym chcesz się ruszyć");
+                Console.WriteLine("2: Podaj jego współrzędne oddzielając je przecinkiem i potwierdz enterem");
+                Console.WriteLine("3: Czerwone pola to miejsca gdzie możesz przesunąć figurę");
+                Console.WriteLine("4: Po wybraniu pola podaj jego współrzędne oddzielając je przecinkiem i potwierdz enterem");
+                Console.WriteLine("5: Tura się zakończy a pionek przesunie w dane miejsce. ");
+                Console.WriteLine("   Jeżeli popełniony zostanie błąd w trakcie wpisywania współrzędnych figury \n   lub pola docelowego zostaniesz o tym poinformowany w dolnej części konsoli.");
+                Console.WriteLine("   Będziesz mógł poprawić wpisane dane pod warunkiem że nie minęło 100s od czasu rozpoczęcia tury.");
+                Console.WriteLine("   Po czasie 100s stracisz ruch. Czas możesz kontrolować w górnej części ekranu. \n   Uwaga aby odświeżyć ekran wystarczy nacisnąć enter");
+                Console.WriteLine("6: Zakonćzenie gry następuję gdy jeden z graczy zbije króla przeciwnikowi:");
+
+                Console.WriteLine("\n\nAby opuścić help naciśnij enter ");
+                Console.ReadLine();
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Brak wyboru \n\nAby opuścić help naciśnij enter ");
+                Console.ReadLine();
+                Console.ReadLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Źle wprowadzone dane! Dozwolone tylko 1 lub 2" + ex.Message);
+        }
+
         Console.Clear();
     }
     private static void edycja_widoku(Stopwatch stoper)
@@ -20,7 +60,7 @@ internal class Program
         //Console.Clear();
         Console.SetCursorPosition(0,0);
         Console.ForegroundColor = ConsoleColor.DarkMagenta;
-        Console.WriteLine($"Pozostały czas: {100-Convert.ToInt32(stoper.Elapsed.TotalSeconds)}s  (aby odświerzyć zerag wciśnij enter)   ");
+        Console.WriteLine($"Pozostały czas: {100-Convert.ToInt32(stoper.Elapsed.TotalSeconds)}s  (aby odświerzyć zerag wciśnij enter)     ");
         Console.ResetColor();
 
     }
@@ -111,13 +151,18 @@ internal class Program
     
     private static void Main(string[] args)
     {
-        Console.WriteLine("Cześć, zagramy w szachy. Żeby poznać zasady wpisz \"help\" lub wciśnij enter żeby zagrać.");
+        Console.WriteLine("Cześć, zagramy w szachy. Żeby poznać zasady wpisz \"help\" lub wybierz tryb gry.");
         Console.WriteLine("Tryb jednoosobowy wciśnij 1 i zatwierdź enterem");
         Console.WriteLine("Tryb dwuosobowy wciśnij 2 i zatwierdź enterem");
         var napis = Console.ReadLine();
         if (napis=="help")
         {
             help();
+            Console.WriteLine("Wybierz tryb gry:");
+            Console.WriteLine("Tryb jednoosobowy wciśnij 1 i zatwierdź enterem");
+            Console.WriteLine("Tryb dwuosobowy wciśnij 2 i zatwierdź enterem");
+            napis = Console.ReadLine();
+            Console.Clear();
         }
         if(napis=="2")
         {   
